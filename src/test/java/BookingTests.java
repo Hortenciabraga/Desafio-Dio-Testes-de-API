29d8f8fddefe5270d7fe6218cf4cc938b7c9e3a2
Hortenciabraga <hort.braga@gmail.com>
2023-01-28 19:20:00 -0300

    Gerando report com Allure

--- src/test/java/BookingTests.java
+++ src/test/java/BookingTests.java
@@ -1,7 +1,20 @@
 import Entities.Booking;
 import Entities.BookingDates;
 import Entities.User;
+import com.github.javafaker.Faker;
+import io.restassured.RestAssured;
+import io.restassured.filter.log.ErrorLoggingFilter;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.*;
 
+import static io.restassured.RestAssured.given;
+import static io.restassured.config.LogConfig.logConfig;
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
+import static org.hamcrest.Matchers.*;
 
 public class BookingTests {
     public static Faker faker;
@@ -86,3 +99,4 @@ public class BookingTests {
     }
 
 }
+
